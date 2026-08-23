d89fc975ea9e6ffe66e6f5f05afaf2f081689009
Robert Wickel <robert.wickel@gmx.de>
2026-08-23 08:53:11 +0200

    fix: show volume bar only on WOD runner page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -188,7 +188,7 @@ const Index: React.FC = () => {
         ) : (
           <div />
         )}
-        {!activeWod && (
+        {activeWod && (
           <div className="ml-auto flex items-center gap-2">
             <AudioToggle
               enabled={audioEnabled}
